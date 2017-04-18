ea66af5f544d7b3703a637e0d75a877c021a89b2
dlutai <dlutai@youa.vn.ua>
2017-04-18 16:03:57 +0300

    added view and index config

--- spring-data-couchbase-2/src/test/resources/docker/configure.sh
+++ spring-data-couchbase-2/src/test/resources/docker/configure.sh
@@ -7,13 +7,16 @@ echo "sleeping..."
 sleep 20
 echo "Start configuring env by calling REST endpoints"
 curl -v -X POST http://localhost:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
-sleep 5
 curl -v -X POST http://localhost:8091/node/controller/setupServices -d services=kv%2Cindex%2Cn1ql
-sleep 5
 curl -i -u Administrator:password -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 curl -v -X POST http://localhost:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 echo "bucket set up start"
 echo "bucket name = " $BUCKET_NAME
 curl -v -X POST http://127.0.0.1:8091/pools/default/buckets -u Administrator:password -d "name="$BUCKET_NAME"" -d 'ramQuotaMB=200' -d 'authType=none' -d 'proxyPort=11222'
+sleep 5
 echo "bucket set up done"
+curl -v http://127.0.0.1:8093/query/service -d "statement=CREATE PRIMARY INDEX ON $BUCKET_NAME"
+cd /opt/couchbase
+#https://developer.couchbase.com/documentation/server/4.6/rest-api/rest-ddocs-create.html
+curl -X PUT -H "Content-Type: application/json" http://Administrator:password@127.0.0.1:8092/baeldung/_design/dev_byfield -d @DeviceIdPhoneToContact.ddoc
 fg 1
